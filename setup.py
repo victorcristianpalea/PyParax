152404dfa272d53b9886419ba1227d6b8390d765
victorcristianpalea <63559097+victorcristianpalea@users.noreply.github.com>
2021-08-24 18:20:40 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ print(setuptools.find_namespace_packages(where='pyparax'))
 
 setuptools.setup(
     name="pyparax",
-    version="0.0.3",
+    version="5.0",
     author="Victor-Cristian Palea",
     author_email="victorcristianpalea@gmail.com",
     description="Optical simulator for paraxial approximation",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
